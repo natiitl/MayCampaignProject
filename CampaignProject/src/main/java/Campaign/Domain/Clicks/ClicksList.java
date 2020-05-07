f04217010425d4e412fc522fc35dabedfde3b8d2
Natalia Terrones <nataliaterrones@lifullconnect.com>
2020-05-07 17:20:38 +0200

    Sprint2: feat: check that if the difference between clicks is greater than fifteen seconds the click not added to the list

--- CampaignProject/src/main/java/Campaign/Domain/Clicks/ClicksList.java
+++ CampaignProject/src/main/java/Campaign/Domain/Clicks/ClicksList.java
@@ -1,7 +1,10 @@
 package Campaign.Domain.Clicks;
 
+import Campaign.Exception.ClickNotValueException;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ClicksList {
     private List<Click> clickList;
@@ -12,12 +15,32 @@ public class ClicksList {
 
 
     public void add(Click newClick) {
-        clickList.add(newClick);
-    }
-    private void compareTimestamps(Click newClick){
-       // return newClick.compareTimes(clickList.get(clickList.size()));
-
+        if(differenceGreaterFifteenSeconds(newClick)) {
+            clickList.add(newClick);
+        }
 
     }
+    private boolean differenceGreaterFifteenSeconds(Click newClick){
 
+        if(clickList.size()==0){
+            return true;
+        }
+       return clickList.get(clickList.size()-1).differenceGreaterFifteenSeconds(newClick);
+
+
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ClicksList that = (ClicksList) o;
+        return Objects.equals(clickList, that.clickList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(clickList);
+    }
 }
--- CampaignProject/src/test/java/UnitTest/Campaing/Campaign/Domain/ClicksListShould.java
+++ CampaignProject/src/test/java/UnitTest/Campaing/Campaign/Domain/ClicksListShould.java
@@ -1,42 +1,37 @@
 package UnitTest.Campaing.Campaign.Domain;
 
 import Campaign.Domain.Clicks.Click;
+import Campaign.Domain.Clicks.ClicksList;
 import Campaign.Domain.Clicks.Premium;
 import Campaign.Domain.User.UserID;
+import Campaign.Exception.ClickNotValueException;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
-
-import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ClicksListShould {
 
     UserID userID;
     Click clickA;
-    Date parsedTimeStamp;
-
-
-    @BeforeEach
-    public void init() throws ParseException {
-        SimpleDateFormat dateFormat = new SimpleDateFormat(
-                "yyyy-MM-dd hh:mm:ss:SSS");
-
-        parsedTimeStamp = dateFormat.parse("2020-07-23 20:56:44:080");
-
-
-        userID = new UserID();
-        clickA = new Click(userID, Premium.PREMIUM, parsedTimeStamp);
-
-    }
 
     @Test
-    public void check_that_two_clicks_do_not_have_the_same_id() {
-        Click clickB = new Click(userID, Premium.PREMIUM, parsedTimeStamp);
+    public void check_that_if_the_difference_between_clicks_is_greater_than_fifteen_seconds_the_click_not_added_to_the_list() throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(
+                "yyyy-MM-dd hh:mm:ss");
+        Date parseDate = dateFormat.parse("2020-07-27 20:50:44");
+        Date parseDate2 = dateFormat.parse("2020-07-27 20:50:45");
+        Click clickA = new Click(userID, Premium.PREMIUM, parseDate);
+        Click clickB = new Click(userID, Premium.PREMIUM, parseDate2);
+        ClicksList clicksListActual = new ClicksList();
+        ClicksList clicksListExpected = new ClicksList();
+        clicksListActual.add(clickA);
+        clicksListActual.add(clickB);
+        clicksListExpected.add(clickA);
 
-        assertEquals(false, clickB.equals(clickA));
+        assertEquals(clicksListExpected,clicksListActual);
     }
 }
